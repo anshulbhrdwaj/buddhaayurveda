a7e8d169197504d6d1195636418fd433da211550
Umang Bhardwaj <umangbhardwaj0006@gmail.com>
2025-01-14 22:52:39 +0530

    Step color fix

--- components/checkout/CheckoutSteps.tsx
+++ components/checkout/CheckoutSteps.tsx
@@ -1,17 +1,26 @@
 const CheckoutSteps = ({ current = 0 }) => {
   return (
-    <ul className='steps steps-vertical mt-4 w-full lg:steps-horizontal'>
+    <ul className="steps steps-vertical mt-4 w-full lg:steps-horizontal">
       {['User Login', 'Shipping Address', 'Payment Method', 'Place Order'].map(
         (step, index) => (
           <li
             key={step}
-            className={`step ${index <= current ? 'step-primary' : ''} `}
+            className={`step ${
+              index < current ? 'step-primary' : '' // Steps before the current
+            } ${
+              index === current ? 'step-warning' : '' // Current step
+            } ${
+              current === 4 && index === 3 ? 'step-warning' : '' // Special case for step 3 in step 4
+            } ${
+              current === 4 && index < 3 ? 'step-primary' : '' // Steps before step 3 in step 4
+            }`}
           >
             {step}
           </li>
-        ),
+        )
       )}
     </ul>
   );
 };
+
 export default CheckoutSteps;
